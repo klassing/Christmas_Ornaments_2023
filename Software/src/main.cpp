dfd842cf477f6b8a73dd790f6cea7593c291c619
klassing <64053662+klassing@users.noreply.github.com>
2023-11-18 11:48:08 -0600

    Stana Sleigh: PCB Outline Finished
    Initial board outline for the santa sleigh controller is completed.

---
+++ Software/src/main.cpp
@@ -0,0 +1,24 @@
+/* 
+    Author: Ryan Klassing 
+    Date: 
+    Version: */ #define SW_VERSION "0.0.1" /*
+    Description:
+
+    License: see "LICENSE" file
+*/
+
+/* ------------ [START] Include necessary libraries -------------- */
+    
+/* -------------- [END] Include necessary libraries -------------- */
+
+/* ------------ [START] Function Prototypes -------------- */
+    
+/* -------------- [END] Function Prototypes -------------- */
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
